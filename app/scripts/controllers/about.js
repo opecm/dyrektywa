ea68ddf00298fe758ee7a08bf48ad642d3ae7db4
Orest Synowiec <orest.synowiec@gmail.com>
2016-03-11 14:41:06 +0100

    first comit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mojaApkaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mojaApkaApp
+ */
+angular.module('mojaApkaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mojaApkaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mojaApkaApp
+ */
+angular.module('mojaApkaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
