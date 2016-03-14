9a8c6870cebffd53406c23bbc5c42d6f32491ed3
Orest Synowiec <orest.synowiec@gmail.com>
2016-03-14 13:48:44 +0100

    surowa wersja

---
+++ app/scripts/controllers/date.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+
+angular.module('mojaApkaApp')
+  .controller('DateCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
